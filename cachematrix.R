11abaa59fd2439d2f0a2707a1fbf8d120bc714e0
neon139 <64305017+neon139@users.noreply.github.com>
2020-05-20 06:41:15 +0530

    changes to find inverse of matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,34 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  set <- function(y) { ## setting the matrix
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x  ##returns the matrix
+  setinverse <- function(inverse) s <<- inverse ##sets the inverse of a matrix
+  getinverse <- function() s ##returns the inverse of a matrix
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  s <- x$getinverse() ##calls the getinverse function
+  if(!is.null(s)) {  ##checks if s is not NULL
+    message("getting cached data") 
+    return(s)  ## returns the cached data
+  }
+  data <- x$get()
+  s <- solve(data, ...)  ##finds the inverse 
+  x$setinverse(s)
+  s
 }
